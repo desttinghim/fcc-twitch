8e56fb89d2a4200132e42cbc7b461d4a23b17322
Louis Pearson <desttinghimgame@gmail.com>
2017-04-03 11:09:24 -0500

    Add basic interface
    Next step, loading data from elsewhere

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
+        streamers: [
+        {name: 'cookie', img: 'http://www.placehold.it/50x50', message: 'offline', status: 'offline'},
+        {name: 'pie', img: 'http://www.placehold.it/50x50', message: 'Streaming cats', status: 'online'},
+        {name: 'cake', img: 'http://www.placehold.it/50x50', message: 'offline', status: 'offline'},
+        ],
     }
 });
+
+// function setData(data) {
+//     data = Json.parse(data);
+//     data['']
+// }
+
+function getStreamerData(name) {
+    ajax({
+        url: 'https://wind-bow.gomix.me/twitch-api/users/$name',
+        callBack: setData
+    });
+    ajax({
+        url: 'https://wind-bow.gomix.me/twitch-api/streams/$name',
+        callBack: setData
+    });
+}
